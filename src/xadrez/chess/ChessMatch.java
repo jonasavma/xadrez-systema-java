01037762f5a80e4c1122ad7bb578c8f83f7f3e14
Jonas Avelino <jonasavma@gmail.com>
2023-12-15 18:26:09 -0300

    BoardExeption and defensive programin rodando com erro

--- src/xadrez/chess/ChessMatch.java
+++ src/xadrez/chess/ChessMatch.java
@@ -30,8 +30,9 @@ public class ChessMatch {
 	}
 	
 	public void initialSetup() {
-		board.palcePiece(new Rook(board,Color.WHITE), new Position(2,1));
-		board.palcePiece(new King(board,Color.BLACK), new Position(0,4));
+		board.placePiece(new Rook(board,Color.WHITE), new Position(2,1));
+		board.placePiece(new King(board,Color.BLACK), new Position(0,4));
+		board.placePiece(new King(board,Color.WHITE), new Position(7,4));
 	}
 
 }
