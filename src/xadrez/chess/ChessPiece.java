955437eed23e54ad56b90a3e440b3c3303a088c1
Jonas Avelino <jonasavma@gmail.com>
2023-12-29 23:24:41 -0300

    posible moves of a piece

--- src/xadrez/chess/ChessPiece.java
+++ src/xadrez/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package xadrez.chess;
 import xadrez.boardgame.Board;
 import xadrez.boardgame.Piece;
 
-public class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece{
 	
 	private Color color;
 
