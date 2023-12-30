60ebe9332431efc458ebcb894b7420365ccce2ac
Jonas Avelino <jonasavma@gmail.com>
2023-12-29 22:37:45 -0300

    Moving pieces

--- src/xadrez/chess/ChessException.java
+++ src/xadrez/chess/ChessException.java
@@ -1,6 +1,8 @@
 package xadrez.chess;
 
-public class ChessException extends RuntimeException {
+import xadrez.boardgame.BoardException;
+
+public class ChessException extends BoardException {
 
 	private static final long serialVersionUID = 1L;
 
